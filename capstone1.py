2e53b144463b669ba599cf85d486e53d42f6ea82
Shubham8452 <53007686+Shubham8452@users.noreply.github.com>
2019-07-17 19:53:21 +0530

    Add files via upload

---
+++ capstone1.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# In[7]:
+
+
+df = pd.read_csv('https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M')
+
+
+# In[5]:
+
+
+dd = pd.read_csv('http://cocl.us/Geospatial_data')
+
+
+# In[6]:
+
+
+dd
+
